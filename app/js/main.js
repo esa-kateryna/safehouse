e5f89a07e5144b7412a9afe69e0fda092cb6849d
esa-kateryna <katerynakov@gmail.com>
2021-07-21 00:03:35 +0300

    home and about added

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $(function(){
 
+    $('.home__slider').slick({
+        dots: true,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        fade: true
+    })
+
 });
