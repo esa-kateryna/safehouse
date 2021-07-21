6bc2f082e757ff0e1f42b8980724349b091aab4a
esa-kateryna <katerynakov@gmail.com>
2021-07-22 00:28:30 +0300

    section service and tabs done

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,14 @@ $(function(){
         autoplay: true,
         autoplaySpeed: 3000,
         fade: true
-    })
+    });
+
+    $('.service-tabs__side-item').on('click', function(e){
+        e.preventDefault();
+        $('.service-tabs__side-item').removeClass('service-tabs__side-item--active');
+        $(this).addClass('service-tabs__side-item--active');
+        $('.service-tabs__content-item').removeClass('service-tabs__content-item--active');
+        $($(this).attr('href')).addClass('service-tabs__content-item--active')
+    });
 
 });
